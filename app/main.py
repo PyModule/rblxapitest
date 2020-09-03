6af8b01304c2dce1f1e7aa379b1a2d235608f689
PyModule <obama8mypie@gmail.com>
2020-09-03 18:36:07 -0400

    Added get users endpoint

--- app/main.py
+++ app/main.py
@@ -32,19 +32,69 @@ async def userlvl(userlvl: userlvl):
     cursor.execute(create_table)
 
 
-    insertion_query = f"""
-    INSERT INTO userlvl (username, level)
-    VALUES
-    {tuple((userlvl.username, userlvl.level))}
+    getuser_query = f"""
+        SELECT
+            *
+        FROM userlvl
+        where userlvl.username = "{userlvl.username}"
     """
-    cursor.execute(insertion_query)
+
+    output = connection.execute(getuser_query).fetchall()
+
+    if output:
+        update_user = f"""
+        UPDATE 
+            userlvl
+        SET
+            level = {userlvl.level}
+        WHERE username = "{userlvl.username}"
+        """
+        cursor.execute(update_user)
+
+        connection.commit()
+
+        connection.close()
+
+        return "User already existed, updated DB"
+    
+    else:
+        insertion_query = f"""
+        INSERT INTO userlvl (username, level)
+        VALUES
+        {tuple((userlvl.username, userlvl.level))}
+        """
+        cursor.execute(insertion_query)
+
+        connection.commit()
+
+        connection.close()
+
+        return "Added to the db"
+
+
+@app.get('/user/{username}')
+async def getuserlvl(username: str):
+
+    connection = sqlite3.connect("lvl.sqlite3")
+
+    cursor = connection.cursor()
+
+    getuser_query = f"""
+        SELECT
+            *
+        FROM userlvl
+        where userlvl.username = "{username}"
+    """
+
+    output = cursor.execute(getuser_query).fetchall()
 
     connection.commit()
     
     connection.close()
+    if not output:
+        return "User was not found in the DB!"
 
-    return "Added to the db?"
-
-
+    else:
+        return {output[0][0]: output[0][1]}
 
 
