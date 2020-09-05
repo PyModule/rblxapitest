d0279b33ac8c89f55cd4df59eb9e9fabca6c3625
PyModule <obama8mypie@gmail.com>
2020-09-04 23:48:15 -0400

    Testing rblxverify

--- app/main.py
+++ app/main.py
@@ -2,17 +2,18 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import sqlite3
 
-class userlvl(BaseModel):
+class verify(BaseModel):
     username: str
-    level: int
+    cordname: str
 
 app = FastAPI()
 
 
 create_table= """
-CREATE TABLE IF NOT EXISTS userlvl (
+CREATE TABLE IF NOT EXISTS verify (
   username text,
-  level integer
+  cordname text,
+  verified text
 );
 """
 
@@ -22,68 +23,40 @@ def root():
     return {"Message": 'Hello World!'}
 
 
-@app.post('/userlvl')
-async def userlvl(userlvl: userlvl):
+@app.post('/verify')
+async def userlvl(verify: verify):
 
-    connection = sqlite3.connect("lvl.sqlite3")
+    connection = sqlite3.connect("verification.sqlite3")
 
     cursor = connection.cursor()
 
     cursor.execute(create_table)
 
-
-    getuser_query = f"""
-        SELECT
-            *
-        FROM userlvl
-        where userlvl.username = "{userlvl.username}"
+    insertion_query = f"""
+    INSERT INTO verify (username, cordname, verified)
+    VALUES
+    {tuple((verify.username, verify.cordname, "No"))}
     """
+    cursor.execute(insertion_query)
 
-    output = connection.execute(getuser_query).fetchall()
+    connection.commit()
 
-    if output:
-        update_user = f"""
-        UPDATE 
-            userlvl
-        SET
-            level = {userlvl.level}
-        WHERE username = "{userlvl.username}"
-        """
-        cursor.execute(update_user)
+    connection.close()
 
-        connection.commit()
+    return "Ready for verification"
 
-        connection.close()
-
-        return "User already existed, updated DB"
-    
-    else:
-        insertion_query = f"""
-        INSERT INTO userlvl (username, level)
-        VALUES
-        {tuple((userlvl.username, userlvl.level))}
-        """
-        cursor.execute(insertion_query)
-
-        connection.commit()
-
-        connection.close()
-
-        return "Added to the db"
-
-
-@app.get('/user/{username}')
+@app.get('/rblxverify/{username}')
 async def getuserlvl(username: str):
 
-    connection = sqlite3.connect("lvl.sqlite3")
+    connection = sqlite3.connect("verification.sqlite3")
 
     cursor = connection.cursor()
 
     getuser_query = f"""
         SELECT
             *
-        FROM userlvl
-        where userlvl.username = "{username}"
+        FROM verify
+        where verify.username = "{username}"
     """
 
     output = cursor.execute(getuser_query).fetchall()
@@ -91,11 +64,13 @@ async def getuserlvl(username: str):
     connection.commit()
     
     connection.close()
+
     if not output:
         return "User was not found in the DB!"
 
     else:
-        return {output[0][0]: output[0][1]}
+        return output[0][1]
+
 
 
 @app.get('/allusers')
